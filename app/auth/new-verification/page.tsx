e407f24d7ed2dd1f244db71f8a2d43a9be3c5478
fvizpal <vishalp9966@gmail.com>
2024-06-03 09:33:19 +0530

    added new-verifcation component andaction

---
+++ app/auth/new-verification/page.tsx
@@ -0,0 +1,9 @@
+import { NewVerificationForm } from "@/components/auth/newVerificationForm"
+
+const NewVerificationPage = () => {
+  return (
+    <NewVerificationForm />
+  );
+}
+
+export default NewVerificationPage;
---
+++ components/auth/newVerificationForm.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useCallback, useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+
+import { LoaderCircle } from "lucide-react";
+import { newVerification } from "@/lib/actions/auth/newVerification";
+import { CardWrapper } from "./cardWrapper";
+import { FormSuccess } from "../shared/formSuccess";
+import { FormError } from "../shared/formError";
+
+export const NewVerificationForm = () => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+
+  const searchParams = useSearchParams();
+
+  const token = searchParams.get("token");
+
+  const onSubmit = useCallback(() => {
+    if (success || error) return;
+
+    if (!token) {
+      setError("Missing token!");
+      return;
+    }
+
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data.success);
+        setError(data.error);
+      })
+      .catch(() => {
+        setError("Something went wrong!");
+      })
+  }, [token, success, error]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
+  return (
+    <CardWrapper
+      headerLabel="Confirming your verification"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
+    >
+      <div className="flex items-center w-full justify-center">
+        {!success && !error && (
+          <LoaderCircle />
+        )}
+        <FormSuccess message={success} />
+        {!success && (
+          <FormError message={error} />
+        )}
+      </div>
+    </CardWrapper>
+  )
+}
