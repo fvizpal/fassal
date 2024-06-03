aea67921329bffa2e16b7bc1196d7b8687e2646b
fvizpal <vishalp9966@gmail.com>
2024-06-04 03:18:29 +0530

    Initialised authjs

--- auth.config.ts
+++ auth.config.ts
@@ -7,6 +7,7 @@ import Google from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
 import { LoginSchema } from "@/schemas";
 import { db } from "./lib/db";
+import { getUserByEmail } from "./lib/data/user";
 
 export default {
   providers: [
@@ -15,6 +16,25 @@ export default {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordsMatch = await bcrypt.compare(
+            password,
+            user.password, // hashed
+          );
+
+          if (passwordsMatch) return user;
+        }
+
+        return null;
+      }
     })
   ],
 } satisfies NextAuthConfig
--- auth.ts
+++ auth.ts
@@ -4,6 +4,18 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { db } from "./lib/db";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: { id: user.id },
+        data: { emailVerified: new Date() }
+      })
+    }
+  },
   adapter: PrismaAdapter(db),
   ...authConfig
 })
