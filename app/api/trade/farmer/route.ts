a7239d30527278e3020e075e7894064da448d8e3
fvizpal <vishalp9966@gmail.com>
2024-06-22 17:50:34 +0530

    image uploader setup

--- app/api/trade/farmer/route.ts
+++ app/api/trade/farmer/route.ts
@@ -1,9 +1,28 @@
-
-
-
+import { currentUser } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
+    const { name, quantity, expectedPrice, imageUrl, location } = await req.json();
+
+    const user = await currentUser();
+
+    if (!user) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const commodity = await db.commodity.create({
+      data: {
+        userId: user.id as string,
+        name,
+        imageUrl,
+        expectedPrice,
+        quantity,
+        location,
+        sold: false,
+      }
+    })
 
   } catch (error) {
 
--- components/modals/CreateCommodity.tsx
+++ components/modals/CreateCommodity.tsx
@@ -10,10 +10,13 @@ import { Input } from "../ui/input";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { Button } from "../ui/button";
+import { FileUpload } from "../shared/fileUploader";
 
 const formSchema = z.object({
   name: z.string().min(1, { message: "Commodity name is required" }),
-  // imageUrl: z.string().min(1, { message: "Commodity image is required" }),
+  imageUrl: z.string().min(1, {
+    message: "Community image is required."
+  }),
   quantity: z.string().min(1, { message: "Quantity is required" }),
   expectedPrice: z.string().min(1, { message: "Enter expected price" }),
 })
@@ -28,7 +31,7 @@ const CreateCommodity = () => {
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: "",
-      // imageUrl: "",
+      imageUrl: "",
       quantity: "",
       expectedPrice: "",
     }
@@ -66,45 +69,64 @@ const CreateCommodity = () => {
         </DialogHeader>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)}>
-            <FormField
-              control={form.control}
-              name="name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Commodity name</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="quantity"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Quantity</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="expectedPrice"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Price</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+            <div className="flex items-center justify-center text-center">
+              <FormField
+                control={form.control}
+                name="imageUrl"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <FileUpload
+                        endpoint="commodityImage"
+                        value={field.value}
+                        onChange={field.onChange}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Commodity name</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="quantity"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Quantity</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="expectedPrice"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Price</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
 
             <DialogFooter className="mt-5">
               <Button disabled={isLoading} className="w-full">
