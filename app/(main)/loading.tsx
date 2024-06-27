9225368c91e389584a8a2a3cf30030700ef2d7f3
fvizpal <vishalp9966@gmail.com>
2024-06-27 23:03:23 +0530

    loading ui added

---
+++ app/(main)/loading.tsx
@@ -0,0 +1,20 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function Loading() {
+  // You can add any UI inside Loading, including a Skeleton.
+  return (
+    <section className="flex gap-5">
+      {
+        Array.from({ length: 30 }).map((_, index) => (
+          <div className="flex flex-col space-y-3 m-5 gap-5" key={index}>
+            <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-[250px]" />
+              <Skeleton className="h-4 w-[200px]" />
+            </div>
+          </div>
+        ))
+      }
+    </section>
+  )
+}
