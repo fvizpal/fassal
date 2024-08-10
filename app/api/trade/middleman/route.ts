1add21c0ef0034b56f5f92c35eb734b89ba80dd7
fvizpal <vishalp9966@gmail.com>
2024-08-10 18:25:55 +0530

    refactor:middleman apis

---
+++ app/api/trade/middleman/route.ts
@@ -0,0 +1,31 @@
+import { currentUser } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const { name, quantity, expectedPrice } = await req.json();
+
+    const user = await currentUser();
+
+    if (!user) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const commodity = await db.commodity.create({
+      data: {
+        userId: user.id as string,
+        name,
+        expectedPrice,
+        quantity,
+        sold: false,
+      }
+    })
+
+    return NextResponse.json(commodity)
+
+  } catch (error) {
+    console.log("[COMMODITY_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
